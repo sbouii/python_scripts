68750c7e413b56faa4582c5e8ba9e6b162eefdc7
sbouii <mariemsbouiii40@gmail.com>
2017-08-22 21:11:23 +0100

    first commit

--- kill_process.py
+++ kill_process.py
@@ -10,7 +10,7 @@ def dispaly_error(message):
    print FAIL + BOLD + UNDERLINE + message + ENDC
 def display_result(message):
    print OKGREEN + BOLD + message + ENDC
-def process_action(func1,func2, mes1, mes2):
+def process_action(func1,func2, mes1, mes2, mes3):
    parser = argparse.ArgumentParser(description='This Python script collects data from files')
    parser.add_argument('-p', '--process', nargs='+', type=str, required=True, help='The name of the process')
    parser.add_argument('-a', '--action', nargs='?', help='The action to take on the process')
@@ -21,14 +21,16 @@ def process_action(func1,func2, mes1, mes2):
        output = subprocess.Popen(['pidof', i], stdout=subprocess.PIPE).communicate()[0] 
        process_id = [str(i) for i in output.split()]      
              
-       if process_id is None:
-        func1(mes2)
-        sys.exit(1)
-       else: 
+       if process_id: 
         for i in process_id:         
           process = subprocess.call(["kill", "-9", i])
+        func2(mes2)
+        print args.process
+       else:
+        func1(mes2)
+        sys.exit(1)
                                     
-      func2(mes2)
+      func2(mes3)
       print args.process
    else:
      func1(mes1)
@@ -36,10 +38,10 @@ def process_action(func1,func2, mes1, mes2):
      print "Aborting ..."
 
 def main():
- message_error = "This python script takes at least two arguments \n "
- message_error = "There is no process with this name, check the process name"
+ message_error1 = "This python script takes at least two arguments \n "
+ message_error2 = "There is no process with this name, check the process name"
  message_result = "*** killed process ***\n"
- process_action(dispaly_error,display_result, message_error, message_result)
+ process_action(dispaly_error,display_result, message_error1, message_error2, message_result)
 
 if __name__ == "__main__":
  main()
