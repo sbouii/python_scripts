feb08ba2001efc1b922a11e332f2f92f2d9f98a5
sbouii <mariemsbouiii40@gmail.com>
2017-08-23 14:50:47 +0100

    first commit

---
+++ check_process.py
@@ -0,0 +1,38 @@
+import subprocess
+import commands
+
+FAIL = '\033[91m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+ENDC = '\033[0m'
+OKGREEN = '\033[92m'
+
+def display_error(message):
+  print FAIL + BOLD + UNDERLINE + message + ENDC
+
+def display_result(message):
+  print OKGREEN + BOLD + message + ENDC
+
+def check_process_status(func1, func2, mes1, mes2):
+   process = raw_input("Enter the name of the process to check: ")
+#  try:
+   output = commands.getoutput("ps -f|grep " + process)
+
+   processinfo = output.split()
+   func2(mes2)
+   print "\n\
+   Full path:\t\t", processinfo[5], "\n\
+   Owner:\t\t", processinfo[0], "\n\
+   Process ID:\t\t", processinfo[1], "\n\
+   Parent process ID:\t", processinfo[2], "\n\
+   Time started:\t", processinfo[4]  
+#  except:
+#   func1(mes1)
+def main():
+ message_error1 = "There is no process with this name" 
+ message_result = "*** Process status: ***"
+ check_process_status(display_error, display_result, message_error1, message_result)
+if __name__ == "__main__":
+ main()
+ 
+
--- kill_process.py
+++ kill_process.py
@@ -6,21 +6,19 @@ BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
 ENDC = '\033[0m'
 OKGREEN = '\033[92m'
-def dispaly_error(message):
+def display_error(message):
    print FAIL + BOLD + UNDERLINE + message + ENDC
 def display_result(message):
    print OKGREEN + BOLD + message + ENDC
 def process_action(func1,func2, mes1, mes2, mes3):
    parser = argparse.ArgumentParser(description='This Python script collects data from files')
    parser.add_argument('-p', '--process', nargs='+', type=str, required=True, help='The name of the process')
-   parser.add_argument('-a', '--action', nargs='?', help='The action to take on the process')
    args = parser.parse_args()
-   if len(sys.argv) < 5:
+   if len(sys.argv) < 7:
      if args.process:
       for i in args.process:       
        output = subprocess.Popen(['pidof', i], stdout=subprocess.PIPE).communicate()[0] 
-       process_id = [str(i) for i in output.split()]      
-             
+       process_id = [str(i) for i in output.split()]             
        if process_id: 
         for i in process_id:         
           process = subprocess.call(["kill", "-9", i])
@@ -41,7 +39,7 @@ def main():
  message_error1 = "This python script takes at least two arguments \n "
  message_error2 = "There is no process with this name, check the process name"
  message_result = "*** killed process ***\n"
- process_action(dispaly_error,display_result, message_error1, message_error2, message_result)
+ process_action(display_error,display_result, message_error1, message_error2, message_result)
 
 if __name__ == "__main__":
  main()
