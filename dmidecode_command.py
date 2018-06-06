71008139993ba99824f065a59c35577d26cef81c
sbouii <mariemsbouiii40@gmail.com>
2018-06-06 17:16:35 +0100

    update dmidecode_command.py script

--- dmidecode_command.py
+++ dmidecode_command.py
@@ -1,17 +1,22 @@
 import subprocess
-import optparse
+import sys
+import argparse
 
 def system_info():
  command = "dmidecode"
- p = optparse.OptionParser(description="Python 'dmidecode' clone")
- p.add_option("-t", "--table", help="Extract hardware information by reading data from the DMI tables")
- options, arguments = p.parse_args()
- 
- print "Gathering %s information..." % options.table
- subprocess.call([command, "-t", options.table])
-    
+ parser = argparse.ArgumentParser(description="Python 'dmidecode' clone")
+ parser.add_argument("-t", "--table", help="Extract hardware information by reading data from the DMI tables")
+ args = parser.parse_args()
+ if args.table:
+   print "Gathering %s information..." % args.table
+   subprocess.call([command, "-t", args.table])
+ else:
+   print "enter the name of the hardware to check"
+   parser.print_help()
+
 def main():
  system_info()
 
 if __name__ == "__main__":
  main()
+~       
